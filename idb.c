bec5fc59350001b939c1570fa9c193f8d594f84a
iaalm <iaalmsimon@gmail.com>
2014-05-02 20:54:31 +0800

    use funnction to replace macro

--- idb.c
+++ idb.c
@@ -1,1 +1,30 @@
-int _idb_malloc_free_c = 0;
+#include <stdio.h>
+#include <stdlib.h>
+static int idb_mf_c = 0;	//malloc free count
+
+void *idb_malloc(size_t size,char* file,unsigned int line){
+	void *t;
+	t = malloc(size);
+	if(t)
+		++idb_mf_c;	//if malloc fail , don't count
+	fprintf(stderr,"%s:%d\tmalloc %d byte(s):%X\n",file,line,size,t);
+	return t;
+}
+void idb_free(void *ptr,char* file,unsigned int line){
+	if(ptr)
+		--idb_mf_c;	//if free null , don't count
+	free(ptr);
+	fprintf(stderr,"%s:%d\tfree %X\n",file,line,ptr);
+}
+
+void idb_mf_assert(int val,char* file,unsigned int line){
+	if(idb_mf_c != val){
+		fprintf(stderr,"%s:%d\tidb_mf_assert fail! idb_mf_c is %d not %d !\n",__FILE__,__LINE__,idb_mf_c,val);
+		exit(-1);
+	}
+}
+
+int idb_mf_count(char* file,unsigned int line){
+	fprintf(stderr,"%s:%d\tidb_mf_count is %d\n",file,line,idb_mf_c);
+	return idb_mf_c;
+}
--- idb.h
+++ idb.h
@@ -1,4 +1,10 @@
-extern int _idb_malloc_free_c;
-#define malloc(arg) (++_idb_malloc_free_c,fprintf(stderr,"%s:%d\tmalloc %d byte(s)\n",__FILE__,__LINE__,arg),malloc(arg))
-#define free(arg) (--_idb_malloc_free_c,fprintf(stderr,"%s:%d\tfree address %d\n",__FILE__,__LINE__,arg),free(arg))
-#define IDB_MALLOC_FREE_BALANCE_ASSERT(arg) (_idb_malloc_free_c == (arg))||(fprintf(stderr,"%s:%d\tIDB_MALLOC_FREE_BALANCE_ASSERT Fail!\n",__FILE__,__LINE__))
+#define malloc(arg) idb_malloc(arg,__FILE__,__LINE__)
+#define free(arg) idb_free(arg,__FILE__,__LINE__)
+#define idb_malloc_free_assert(arg) idb_mf_assert(arg,__FILE__,__LINE__)
+#define idb_all_free() idb_mf_assert(0,__FILE__,__LINE__)
+#define idb_malloc_free_count() idb_mf_count(__FILE__,__LINE__)
+
+void *idb_malloc(size_t,char*,unsigned int);
+void idb_free(void *,char*,unsigned int);
+void idb_mf_assert(int,char*,unsigned int);
+int idb_mf_count(char*,unsigned int);
